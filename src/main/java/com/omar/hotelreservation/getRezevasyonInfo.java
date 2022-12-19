5f3c293c16da5d80d302b019faa08a8392bc5479
OAMR ALFAROUK ALMOHAMAD <teknopluse12@gmail.com>
2022-12-19 08:29:51 +0300

    modify indexPage, loginPage add reverasionPage and userPAGE

---
+++ src/main/java/com/omar/hotelreservation/getRezevasyonInfo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.omar.hotelreservation;
+
+/**
+ *
+ * @author hakkimertpeyk
+ */
+public class getRezevasyonInfo {
+    String girisTarihi;
+    String cikisTarihi;
+    String oda;
+    String misafir;
+    
+    
+    
+    public void setGirisTarihi(String girisTarihi){
+        this.girisTarihi = girisTarihi;
+    }
+    public String getGirisTarihi(){
+        return girisTarihi;
+    }
+    
+    public void setCikisTarihi(String cikisTarihi){
+        this.cikisTarihi = cikisTarihi;
+    }
+    public String getCikisTarihi(){
+        return cikisTarihi;
+    }
+    
+    public void setOda(String oda){
+        this.oda = oda;
+    }
+    public String getOda(){
+        return oda;
+    }
+    
+     public void setMisafir(String misafir){
+        this.misafir = misafir;
+    }
+    public String getMisafir(){
+        return misafir;
+    }
+    
+    
+}
+
